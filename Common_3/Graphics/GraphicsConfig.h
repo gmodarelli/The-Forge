6c0498260d2457687475d31b288f4d8f585f61f7
Anders Elfgren <srekel@gmail.com>
2024-03-11 06:50:26 +0100

    Added zig bindings for various headers.

--- Common_3/Graphics/GraphicsConfig.h
+++ Common_3/Graphics/GraphicsConfig.h
@@ -47,8 +47,10 @@
 #ifndef _WINDOWS7
 #include "Direct3D12/Direct3D12Config.h"
 #endif
+#if !defined(TIDES)
 #include "Direct3D11/Direct3D11Config.h"
 #include "Vulkan/VulkanConfig.h"
+#endif
 #elif defined(XBOX)
 #include "Direct3D12/Direct3D12Config.h"
 #elif defined(__APPLE__)
