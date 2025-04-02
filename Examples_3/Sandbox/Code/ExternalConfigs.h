8017fa74ccf2bb78243acc66aa3d8003ecd1c003
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2025-04-02 21:35:19 +0200

    Sandbox example
    We're able to initialize the renderer and a graphics queue

---
+++ Examples_3/Sandbox/Code/ExternalConfigs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#include "../../../Common_3/Graphics/Direct3D12/Direct3D12Config.h"
+
+#define VALIDATE_DESCRIPTOR(descriptor, ...)
+#define IF_VALIDATE_DESCRIPTOR(...)
+#define IF_VALIDATE_DESCRIPTOR_MEMBER(T, Name)
