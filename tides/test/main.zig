c0b8297c3998a4f41815027a5be7178bbe636f92
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2025-04-15 06:59:28 +0200

    Test: working renderer initialization

--- tides/test/main.zig
+++ tides/test/main.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
 const zf = @import("ze_forge");
-const graphics = zf.IGraphics;
+const zgpu = zf.IGraphics;
+const zgpu_tides = zf.IGraphicsTides;
+
+pub export const D3D12SDKVersion: u32 = 715;
+pub export const D3D12SDKPath: [*:0]const u8 = ".\\";
 
 pub fn main() !void {
-    std.debug.print("lol", .{});
-    var renderer_desc = std.mem.zeroes(graphics.RendererDesc);
-    var renderer: [*c]graphics.Renderer = null;
-    zf.IGraphics.initRenderer("ze_forge test", &renderer_desc, &renderer);
-    std.debug.print("lol", .{});
+    var renderer_desc = std.mem.zeroes(zgpu.RendererDesc);
+    var renderer: [*c]zgpu.Renderer = null;
+    zgpu_tides.initGPUConfigurationEx(renderer_desc.pExtendedSettings);
+    defer zgpu_tides.exitGPUConfigurationEx();
+    zgpu.initRenderer("ze_forge test", &renderer_desc, &renderer);
+    defer zgpu.exitRenderer(renderer);
 }
