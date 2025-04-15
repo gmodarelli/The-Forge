0697b737a44e2a641b472426cc8c3b2ddae2a081
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2025-04-15 06:58:18 +0200

    Interfaces: fix memory allocations (WIP)

--- Common_3/Tides/WindowsMemory.c
+++ Common_3/Tides/WindowsMemory.c
@@ -1,15 +1,11 @@
 #define IMEMORY_FROM_HEADER
 #include "../Utilities/Interfaces/IMemory.h"
 
-#include <memory.h>
+#include <Windows.h>
 
 void* tf_malloc_internal(size_t size, const char* f, int l, const char* sf)
 {
-    (void)f;
-    (void)l;
-    (void)sf;
-
-    return malloc(size);
+    return tf_memalign_internal(16, size, f, l, sf);
 }
 
 void* tf_realloc_internal(void* ptr, size_t size, const char* f, int l, const char* sf)
@@ -18,7 +14,8 @@ void* tf_realloc_internal(void* ptr, size_t size, const char* f, int l, const ch
     (void)l;
     (void)sf;
 
-    return realloc(ptr, size);
+    void* new_ptr = _aligned_realloc(ptr, size, 16);
+    return new_ptr;
 }
 
 void tf_free_internal(void* ptr, const char* f, int l, const char* sf)
@@ -27,16 +24,12 @@ void tf_free_internal(void* ptr, const char* f, int l, const char* sf)
     (void)l;
     (void)sf;
 
-    free(ptr);
+    _aligned_free(ptr);
 }
 
 void* tf_calloc_internal(size_t count, size_t size, const char* f, int l, const char* sf)
 {
-    (void)f;
-    (void)l;
-    (void)sf;
-
-    return calloc(count, size);
+    return tf_calloc_memalign_internal(count, 16, size, f, l, sf);
 }
 
 void* tf_memalign_internal(size_t align, size_t size, const char* f, int l, const char* sf)
@@ -44,10 +37,10 @@ void* tf_memalign_internal(size_t align, size_t size, const char* f, int l, cons
     (void)f;
     (void)l;
     (void)sf;
-    (void)align;
 
-    // TODO: align allocation
-    return malloc(size);
+    void* ptr = _aligned_malloc(size, align);
+    memset(ptr, 0, size);
+    return ptr;
 }
 
 void* tf_calloc_memalign_internal(size_t count, size_t align, size_t size, const char* f, int l, const char* sf)
@@ -55,8 +48,9 @@ void* tf_calloc_memalign_internal(size_t count, size_t align, size_t size, const
     (void)f;
     (void)l;
     (void)sf;
-    (void)align;
 
-    // TODO: align allocation
-    return calloc(count, size);
+    size_t new_size = count * size;
+    void* ptr = _aligned_malloc(new_size, align);
+    memset(ptr, 0, new_size);
+    return ptr;
 }
