1f64612c27ac996cbf257f8a1a03d23522197d3c
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2025-04-15 06:59:13 +0200

    ze-forge: expose IGraphicsTides

--- root.zig
+++ root.zig
@@ -1,3 +1,3 @@
 pub const IGraphics = @import("Common_3/Graphics/Interfaces/IGraphics.zig");
-// pub const IGraphicsTides = @import("Common_3/Graphics/Interfaces/IGraphicsTides.zig");
+pub const IGraphicsTides = @import("Common_3/Graphics/Interfaces/IGraphicsTides.zig");
 // pub const IRay = @import("Common_3/Graphics/Interfaces/IRay.zig");
